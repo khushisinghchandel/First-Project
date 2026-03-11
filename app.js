7aebfaa2162c8b5dff50ae2d7c55eef0b8cf1699
Khushi Chandel <161255868+khushisinghchandel@users.noreply.github.com>
2026-03-11 15:54:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const sessionOptions = {
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    ccokie: {
+    cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true
